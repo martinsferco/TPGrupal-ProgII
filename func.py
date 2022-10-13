9e0429b6e098048ab1546bc080d88ec95610e129
Valenchu <68815558+fasiluva@users.noreply.github.com>
2022-10-12 21:56:39 -0300

    Sintesis de pasos a seguir

--- func.py
+++ func.py
@@ -1,1 +1,28 @@
 # func.py
+
+"""
+Variables utilizadas:
+
+Ficha: Tuple
+Tablero: List(Ficha)
+
+Listado de funciones para hacer: 
+
+. Inicializar tablero
+. Determinar jugadores, que color le corresponde y quien inicia
+. Escribir jugadas
+    | Chequear que la ficha este dentro del tablero
+    | Chequear que la ficha este en una posicion vacia
+    | Chequear que la ficha este tangente a una ficha del otro color
+    | Chequear que la ficha colocada encierre fichas del otro color
+        || Horizontalmente
+        || Verticalmente
+        || Diagonalmente
+    | Modificar el tablero, y repetir
+. Mostrar el tablero
+. Determinar el ganador (si es que lo hay)
+
+P.D: Si un chequeo del paso escribir jugadas no se cumple, sale de la funcion,
+especifica cual fue el error, muestra el tablero y termina.
+
+"""
